9ab7cbaa29850a742f3d0789c45c94d48924ba9b
Aastha <aasthakushwaha552@gmail.com>
2026-06-10 14:25:15 +0530

    create college table and insert student records
    use of WHERE

--- college.sql
+++ college.sql
@@ -7,3 +7,11 @@ CREATE table college (
 );
 
 INSERT INTO college( Roll Number,  name, Marks,Grade,City)VALUE
+(1,"sahil",59,"D","GUJRAT"),
+(2,"divyanshu",89,"A","DELHI"),
+(3,"arpita",67,"C","JHARKAND"),
+(4,"yashita",87,"A","LUCKNOW"),
+(5,"sonali",82,"B","MEEERUT");
+
+SELECT Marks FROM college WHERE Marks>65;
+SELECT*FROM college
